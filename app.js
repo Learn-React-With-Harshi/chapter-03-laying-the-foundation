a7d78d6f297bc35c449711014ff60ccddfd1fa0f
Harshitha SV <solaiharshitha0@gmail.com>
2023-01-04 20:23:01 +0100

    Updated code

--- app.js
+++ app.js
@@ -7,8 +7,7 @@ const heading1 = React.createElement("h1", {id: "heading"}, "Namaste React Live"
 const heading2 = React.createElement("h2", {id: "main_heading"}, "Chapter 03 : Laying the foundation");
 const heading3 = React.createElement("h3", {id: "sub_heading"}, "Nested header element with h1,h2,h3 using React.createElement()");
 
-const header = React.createElement("div", {class : "title"}, [heading1, heading2, heading3]);
-
+const header = React.createElement("div", {className : "title"}, [heading1, heading2, heading3]);
 
 /* Nested header element with h1,h2,h3 using JSX */ 
 const jsx_header= (
@@ -19,40 +18,25 @@ const jsx_header= (
   </div>
 );
 
-/* Nested header element with h1,h2,h3 using JSX React Element */ 
+/* Nested header element with h1,h2,h3 using JSX React Element and pass attributes */ 
 const jsx_header_attr= (
   <div className="title">
-    <h1> <a href="https://learn.namastedev.com/">Namaste React Live </a></h1>
+    <h1 id="heading1" titlename="namaste"> <a href="https://learn.namastedev.com/">Namaste React Live </a></h1>
     <h2>Chapter 03 : Laying the foundation</h2>
-    <h3>Nested header element with h1,h2,h3 using JSX</h3>
+    <h3>Nested header element with h1,h2,h3 using JSX and pass attributes</h3>
   </div>
 );
 
-
 /* Nested header element with h1,h2,h3 using Functional Component */ 
-const HeadingComponent = function() {
+const TitleComponent = function() {
   return (
     <div className="title">
-      { TitleComponent } {/* It does not work since TitleComponent is a component and not a react element*/}
-      { <TitleComponent /> } {/* It works since TitleComponent is enclosed within <> */}
-      { <TitleComponent> </TitleComponent> } {/* It works since TitleComponent is enclosed within <> */}
-      <h1><a href="https://learn.namastedev.com/">Namaste React Live</a></h1>
+      <h1>Namaste React Live</h1>
       <h2>Chapter 03 : Laying the foundation</h2>
-      <h3>Nested header element with h1, h2, h3 using Functional Component</h3>
+      <h3>Nested header element with h1, h2, h3 using Functional Component - TitleComponent</h3>
     </div>
   );
 }
-
-
-
-/* Component Composition */
-
-const TitleComponent = function() {
-  return (
-    <h1>Namaste React Live : From Title Component </h1>
-  );
-}
-
 
 /* Assignment 2 */
 
@@ -65,25 +49,39 @@ const HeaderComponent = function (){
       </div>
 
       <div className="search">
-        <input type="text" key="search" placeHolder="Search for a chapter"/>
+        <input type="text" key="search" placeholder="Search for a chapter"/>
       </div>
     
-      <div class="avatar">
-        <div class="user-icon" key="user-icon"> </div>
+      <div className="avatar">
+        <div className="user-icon" key="user-icon"> </div>
       </div>
     </div>
   );
 }
 
+/* Component Composition */
 
+const NestedComponent = function () {
+  return (
+    <div>
+      <HeaderComponent />
+      {header}
+      {jsx_header}
+      {jsx_header_attr}
+      { TitleComponent }{/* It does not work and warning is thrown in console since TitleComponent is a component and not a react element*/}
+      { <TitleComponent /> } {/* It works since TitleComponent is enclosed within <> */}
+      { <TitleComponent> </TitleComponent> } {/* It works since TitleComponent is enclosed within <> */}
+      
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 /* Assignment 1 */
-root.render(header); /* Rendering header created using React.createElement() */
-root.render(jsx_header); /* Rendering header created using JSX */
-root.render(jsx_header_attr); /* Rendering header created using JSX  and passing attributes in jsx tag*/
-root.render(<HeadingComponent />); /* Rendering header created using Functional Component */
+// root.render(header); /* Rendering header created using React.createElement() */
+// root.render(jsx_header); /* Rendering header created using JSX */
+// root.render(jsx_header_attr); /* Rendering header created using JSX  and passing attributes in jsx tag*/root.render(<TitleComponent />); /* Rendering header created using Functional Component */
 
+root.render(<NestedComponent />); /* Assignment 2  */
 
-//root.render(<HeaderComponent />); /* Assignment 2  */
